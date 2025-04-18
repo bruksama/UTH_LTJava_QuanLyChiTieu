de4cc69a1b4d8433a37f3248de8710320f475c6f
Nguyễn Tấn Lộc <42836598+bruksama@users.noreply.github.com>
2025-04-18 23:26:05 +0700

    cài đặt javafx điều chỉnh CategoryService

---
+++ src/main/java/com/expensemanager/Application.java
@@ -0,0 +1,15 @@
+package main.java.com.expensemanager;
+
+import javafx.stage.Stage;
+
+public class Application extends javafx.application.Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
