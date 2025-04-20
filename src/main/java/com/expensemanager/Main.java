c8861e51242de2146d2bca67e52fe57277de8140
Nguyễn Tấn Lộc <42836598+bruksama@users.noreply.github.com>
2025-04-20 21:43:48 +0700

    Thay hàm Main dùng để khởi chạy Application

--- src/main/java/com/expensemanager/Main.java
+++ src/main/java/com/expensemanager/Main.java
@@ -16,37 +16,37 @@ public class Main {
 //        new File(appDataPath + "\\QuanLyChiTieu").mkdirs();
 //
 //        ConnectorDAO dbConnector = ConnectorDAO.getInstance(dbURL);
-        ConnectorDAO dbConnector = ConnectorDAO.getInstance();
-        dbConnector.initDatabase();
-
-//        if (dbConnector.isConnected()) {
-//            System.out.println("Kết nối thành công!");
+//        ConnectorDAO dbConnector = ConnectorDAO.getInstance();
+//        dbConnector.initDatabase();
+//
+////        if (dbConnector.isConnected()) {
+////            System.out.println("Kết nối thành công!");
+////        } else {
+////            System.out.println("Không thể kết nối!");
+////        }
+////
+//        ProfileDAO dbProfile = new ProfileDAO();
+//        Profile testProfile = dbProfile.getProfileById(4);
+//        System.out.println("Đang tương tác bằng profile: " + testProfile.getName());
+////        if (dbProfile.insertProfile(testProfile)) {
+////            System.out.println("Success");
+////        } else {
+////            System.out.println("Fail");
+////        }
+//
+//        CategoryDAO dbCategory = new CategoryDAO();
+////        CategoryService dbCategoryService = new CategoryService(dbCategory);
+////        Category testCategory = dbCategory.getCategoryById(3);
+////
+////        System.out.println("Danh mục " + testCategory.getName());
+////
+//        if(dbCategory.preCreateCategory(testProfile.getId())) {
+//            System.out.println("Thành công");
 //        } else {
-//            System.out.println("Không thể kết nối!");
-//        }
-//
-        ProfileDAO dbProfile = new ProfileDAO();
-        Profile testProfile = dbProfile.getProfileById(4);
-        System.out.println("Đang tương tác bằng profile: " + testProfile.getName());
-//        if (dbProfile.insertProfile(testProfile)) {
-//            System.out.println("Success");
-//        } else {
-//            System.out.println("Fail");
+//            System.out.println("Thất bại");
 //        }
 
-        CategoryDAO dbCategory = new CategoryDAO();
-//        CategoryService dbCategoryService = new CategoryService(dbCategory);
-//        Category testCategory = dbCategory.getCategoryById(3);
-//
-//        System.out.println("Danh mục " + testCategory.getName());
-//
-        if(dbCategory.preCreateCategory(testProfile.getId())) {
-            System.out.println("Thành công");
-        } else {
-            System.out.println("Thất bại");
-        }
-
-//        Application.launch(Application.class);
+        Application.launch(Application.class);
 
     }
 }
